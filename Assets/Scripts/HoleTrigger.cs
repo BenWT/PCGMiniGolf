afa959553c896f33ef8f0f5d18c9eb318e6b3e1a
Ben Townshend <bentownshend@icloud.com>
2017-11-17 10:53:42 +0000

    added first level

--- Assets/Scripts/HoleTrigger.cs
+++ Assets/Scripts/HoleTrigger.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class HoleTrigger : MonoBehaviour {
 	public HoleController hole;
 
+	void Update() {
+		if (!hole) {
+			hole = transform.parent.GetComponent<HoleController>();
+		}
+	}
+
 	void OnTriggerEnter (Collider other) {
 		if (other.tag == "Ball") {
 			hole.InHole();
